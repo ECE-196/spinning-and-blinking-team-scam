5fb09dd7c619d1cb3ca0ebbea3de6191b7d823b6
sbulatao <114209345+sbulatao@users.noreply.github.com>
2024-10-14 22:15:59 -0700

    Given code for Arduino

---
+++ main.ino
@@ -0,0 +1,34 @@
+// const unsigned int LED{17};
+// # add these
+// const unsigned int MTR_HI{13};
+// const unsigned int MTR_LO{14};
+                          
+// void setup() {
+//     pinMode(LED, OUTPUT);
+//     # and these
+//     pinMode(MTR_HI, OUTPUT);
+//     pinMode(MTR_LO, OUTPUT);
+//     # configure pins to spin the motor in a direction
+//     digitalWrite(MTR_HI, HIGH);
+//     digitalWrite(MTR_LO, LOW);
+// }
+
+// void loop() {}
+
+
+// const unsigned int LED{17};
+// # add these
+// const unsigned int MTR_HI{13};
+// const unsigned int MTR_LO{14};
+                          
+// void setup() {
+//     pinMode(LED, OUTPUT);
+//     # and these
+//     pinMode(MTR_HI, OUTPUT);
+//     pinMode(MTR_LO, OUTPUT);
+//     # configure pins to spin the motor in a direction
+//     digitalWrite(MTR_HI, HIGH);
+//     digitalWrite(MTR_LO, LOW);
+// }
+
+// void loop() {}
