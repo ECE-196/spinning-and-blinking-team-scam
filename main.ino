9a14d147d696d10400929bef464c45f6240e975f
Thao <158249497+ThaoChung@users.noreply.github.com>
2024-10-21 15:43:03 -0700

    blinking and spinning code.ino

--- main.ino
+++ main.ino
@@ -1,34 +1,34 @@
-// const unsigned int LED{17};
-// # add these
-// const unsigned int MTR_HI{13};
-// const unsigned int MTR_LO{14};
-                          
-// void setup() {
-//     pinMode(LED, OUTPUT);
-//     # and these
-//     pinMode(MTR_HI, OUTPUT);
-//     pinMode(MTR_LO, OUTPUT);
-//     # configure pins to spin the motor in a direction
-//     digitalWrite(MTR_HI, HIGH);
-//     digitalWrite(MTR_LO, LOW);
-// }
+const unsigned int LED_PIN = 17;
+const unsigned int MOTOR_HIGH_PIN = 13;
+const unsigned int MOTOR_LOW_PIN = 14;
 
-// void loop() {}
+void setup() {
+    // Initialize the LED pin
+    pinMode(LED_PIN, OUTPUT);
+    
+    // Initialize motor pins
+    pinMode(MOTOR_HIGH_PIN, OUTPUT);
+    pinMode(MOTOR_LOW_PIN, OUTPUT);
+}
 
+void loop() {
+    // Blink and spin motor in one direction
+    blinkAndSpin(MOTOR_HIGH_PIN, MOTOR_LOW_PIN);
 
-// const unsigned int LED{17};
-// # add these
-// const unsigned int MTR_HI{13};
-// const unsigned int MTR_LO{14};
-                          
-// void setup() {
-//     pinMode(LED, OUTPUT);
-//     # and these
-//     pinMode(MTR_HI, OUTPUT);
-//     pinMode(MTR_LO, OUTPUT);
-//     # configure pins to spin the motor in a direction
-//     digitalWrite(MTR_HI, HIGH);
-//     digitalWrite(MTR_LO, LOW);
-// }
+    // Blink and spin motor in the opposite direction
+    blinkAndSpin(MOTOR_LOW_PIN, MOTOR_HIGH_PIN);
+}
 
-// void loop() {}
+void blinkAndSpin(unsigned int pin1, unsigned int pin2) {
+    for (int speed = 0; speed <= 255; speed += 5) {
+        // Blink the LED every 25 increments of speed
+        digitalWrite(LED_PIN, (speed % 25 == 0) ? HIGH : LOW);
+
+        // Control the motor speed
+        analogWrite(pin1, speed);
+        analogWrite(pin2, 0);
+
+        // Delay to control the speed of the loop
+        delay(50);
+    }
+}
